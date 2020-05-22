08fe05a0ad577142258c0af69b2a1b0856167d30
Alius <alexa.glinsoo@gmail.com>
2020-05-22 19:07:29 -0300

    pegando a primeira img

--- pokemonDetonados/spiders/detonados.py
+++ pokemonDetonados/spiders/detonados.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from pokemonDetonados.items import DetonadosItem
 
 
 class DetonadosSpider(scrapy.Spider):
     name = 'detonados'
     allowed_domains = ['https://pokemythology.net/detonados/']
-    start_urls = ['http://https://pokemythology.net/detonados//']
+    start_urls = ['https://pokemythology.net/detonados/']
 
     def parse(self, response):
-        pass
+
+        for item in response.css('table'):
+            detonado = item.css('tbody tr')
+            # images
+            images = []
+            image1 = detonado.css('td p img::attr(src)')[0].get()
+            image2 = detonado.css('td p img').get()
+            images.append(image1)
+            images.append(image2)
+            detonado = DetonadosItem(images=images)
+            yield detonado
