f258f807fc234b842f4f00f74a47919eb45f3e93
Alius <alexa.glinsoo@gmail.com>
2020-05-22 20:07:49 -0300

    pegando todos os detonados com seus dados

--- pokemonDetonados/spiders/detonados.py
+++ pokemonDetonados/spiders/detonados.py
@@ -9,14 +9,24 @@ class DetonadosSpider(scrapy.Spider):
     start_urls = ['https://pokemythology.net/detonados/']
 
     def parse(self, response):
-
         for item in response.css('table'):
             detonado = item.css('tbody tr')
+            # montando dados
+            image1 = detonado.css('td')[0]
+            dados = detonado.css('td')[1]
+            image2 = detonado.css('td')[2]
+            linkCod = dados.css('p')[1]
             # images
             images = []
-            image1 = detonado.css('td p img::attr(src)')[0].get()
-            image2 = detonado.css('td p img').get()
-            images.append(image1)
-            images.append(image2)
-            detonado = DetonadosItem(images=images)
-            yield detonado
+            images.append(image1.css('p img::attr(src)').get())
+            images.append(image2.css('p img::attr(src)').get())
+            #
+            title = dados.css('p strong span::text').get()
+            link = linkCod.css('a::attr(href)').get()
+            # se não vier o link inteiro
+            if link[0] == '/':
+                link = 'https://pokemythology.net/detonados' + link
+            # se title for nulo
+            if title is not None:
+                detonado = DetonadosItem(images=images, title = title, link = link)
+                yield detonado
