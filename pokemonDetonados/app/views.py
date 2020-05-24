4bb0e8e409dd04f2226466f98702ce0e32ad7872
Alius <alexa.glinsoo@gmail.com>
2020-05-24 18:55:25 -0300

    colocando flask

---
+++ pokemonDetonados/app/views.py
@@ -0,0 +1,18 @@
+from pokemonDetonados.app import app
+from flask import jsonify
+import json
+
+@app.route('/detonado')
+def getDetonados():
+    detonados = getJSON('app/../detonados.json')
+    return jsonify(detonados)
+
+@app.route('/spinoff')
+def getSpinOff():
+    spinoff = getJSON('app/../spinOff.json')
+    return jsonify(spinoff)
+
+def getJSON(path):
+   with open(path, 'r') as f:
+    return json.load(f)
+ 
